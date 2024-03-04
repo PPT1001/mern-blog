c0e36752da30441535a676f7d0297a9bafb87ef2
PPT1001 <pragash1001@gmail.com>
2024-03-04 14:21:45 +0530

    UserContext part

--- api/index.js
+++ api/index.js
@@ -34,12 +34,16 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
+  console.log(userDoc.password);
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     //Logged In
     jwt.sign({ username, id: userDoc.id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie("token", token).json("ok");
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
     });
   } else {
     res.status(400).json("Wrong Credentials");
@@ -52,6 +56,10 @@ app.get("/profile", (req, res) => {
     if (err) throw err;
     res.json(info);
   });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
 app.listen(4000);
