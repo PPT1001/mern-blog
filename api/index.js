96a2aba4c59e32acb46451e0661d983b13d8d2e8
PPT1001 <pragash1001@gmail.com>
2024-03-04 20:50:12 +0530

    Creating Blog Posts function

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
@@ -60,6 +64,31 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+app.post("/newPost", uploadMiddleware.single("image"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.rename(path, newPath, (err) => {
+    if (err) throw err;
+  });
+
+  const { title, summary, content } = req.body;
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    image: newPath,
+  });
+
+  res.json(postDoc);
+});
+
+app.get("/post", async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
 });
 
 app.listen(4000);
--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import IndexPage from "./pages/indexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path={"/register"} element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
         </Route>
       </Routes>
     </UserContextProvider>
