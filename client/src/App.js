c69391cb189159ece292f93d90914be416f57ffa
PPT1001 <pragash1001@gmail.com>
2024-03-04 12:04:19 +0530

    Update App.js and other components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,21 @@
-import './App.css';
-import Header from './Header';
-import Layout from './Layout';
-import Post from './Post';
-import {Route, Routes} from 'react-router-dom';
-
+import "./App.css";
+// import Header from './Header';
+import Layout from "./Layout";
+// import Post from './Post';
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/indexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
-      <Route path = '/' element={<Layout  />}>
-        <Route index element={
-          <Post />
-        } />
-        <Route path={'/login'} element={
-          <div>Login page</div>
-        } />
-        <Route path={'/register'} element={
-          <div>Register</div>
-        } />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path={"/register"} element={<RegisterPage />} />
       </Route>
     </Routes>
-
-
-      
   );
 }
 
